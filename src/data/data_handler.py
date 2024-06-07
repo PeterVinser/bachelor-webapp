1719a42fe5293781f833fd870a457e97f7c75599
PiotrOk <piotr.okninski800@gmail.com>
2024-06-07 15:01:23 +0200

    randomization issue fixed

--- src/data/data_handler.py
+++ src/data/data_handler.py
@@ -1,16 +1,13 @@
 import streamlit as st
 from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 import certifi
 
-@st.cache_resource
 def init_client():
     return MongoClient(st.secrets["mongo_uri"], tlsCAFile=certifi.where())
 
 client = init_client()
 database = client.get_database("bachelor_db")
 
-@st.cache_data()
 def get_passages():
     collection = database.get_collection("passage")
 
